a557bc7f5ce8bb9ebf452cf2d8ce89d811a93972
Alwan Farhan Aziz <155723244+1705Alwwwan@users.noreply.github.com>
2025-09-04 22:23:36 +0700

    upgrade
    upgrade

---
+++ js/certificates.js
@@ -0,0 +1,80 @@
+const certificates = [
+    {
+        title: "Network Support & Security",
+        issuer: "Cisco Network Academy (CNA)",
+        date: "August 2025",
+        image: "media/img/netsec.jpg",
+        link: "https://www.netacad.com/certificates?issuanceId=65f9a990-343b-47b7-861f-a4eabd4644c7"
+    },
+    {
+        title: "Network Basics",
+        issuer: "Cisco Network Academy (CNA)",
+        date: "July 2025",
+        image: "media/img/netbas.jpg",
+        link: "https://www.netacad.com/certificates?issuanceId=dcb0a597-fe6e-4702-a9d4-bf6d49d51138"
+    },
+    {
+        title: "Python Lanjutan",
+        issuer: "Skilvul",
+        date: "January 2024",
+        image: "media/img/python.jpg",
+        link: "https://skilvul.com/courses/python-lanjutan/student/clpe1ko3a0fzb01lddmjz924u/"
+    },
+    {
+        title: "Cyber Security : From Beginner to Expert",
+        issuer: "Udemy : Logx Academy",
+        date: "July 2025",
+        image: "media/img/cybersec.jpg",
+        link: "https://www.udemy.com/certificate/UC-05d29bb5-987c-499d-9ada-d548f42e2db7/"
+    },
+    {
+        title: "Understanding EU Cyber Resilience Act (CRA) (LFEL1001)",
+        issuer: "The Linux Foundation",
+        date: "July 2025",
+        image: "media/img/cyberresi.jpg",
+        link: "https://ti-user-certificates.s3.amazonaws.com/e0df7fbf-a057-42af-8a1f-590912be5460/aafb2ae9-a902-44cb-b8bb-6d492ca9269a-alwan-aziz-c53d3677-0f04-45c5-afb5-37c86655fc98-certificate.pdf"
+    },
+    {
+        title: "Japan Foundation Test (JFT) - A2",
+        issuer: "Japan Foundation",
+        date: "January 2021",
+        image: "media/img/jft.jpg",
+        link: "#"
+    },
+    {
+        title: "English Skill (TOEFL 550)",
+        issuer: "Sekolah Tinggi Bahasa Asing - JIA",
+        date: "August 2020",
+        image: "media/img/toefl.jpg",
+        link: "#"
+    },
+    {
+        title: "IT Specification Basics for New Developers (LFD141)",
+        issuer: "The Linux Foundation",
+        date: "July 2025",
+        image: "media/img/itspec.jpg",
+        link: "https://ti-user-certificates.s3.amazonaws.com/e0df7fbf-a057-42af-8a1f-590912be5460/aafb2ae9-a902-44cb-b8bb-6d492ca9269a-alwan-aziz-c91d0153-ac98-4740-a917-419b2d88e925-certificate.pdf"
+    },
+    {
+        title: "XSS Exploits and Defenses (LFEL1010)",
+        issuer: "The Linux Foundation",
+        date: "July 2025",
+        image: "media/img/xssexploit.jpg",
+        link: "https://ti-user-certificates.s3.amazonaws.com/e0df7fbf-a057-42af-8a1f-590912be5460/aafb2ae9-a902-44cb-b8bb-6d492ca9269a-alwan-aziz-3532f0b3-01dc-46fe-9225-23c040466c16-certificate.pdf"
+    },
+    {
+        title: "SQL",
+        issuer: "SoloLearn",
+        date: "September 2022",
+        image: "media/img/sql.jpg",
+        link: "https://www.sololearn.com/en/profile/26915424"
+    },
+    {
+        title: "Python Django : Ultimate Beginners Course - 2025",
+        issuer: "Udemy : Arno Pretorius",
+        date: "July 2025",
+        image: "media/img/pythondjango.jpg",
+        link: "https://www.udemy.com/certificate/UC-35bf8132-4222-48d0-ae36-308ba84a6f5d/"
+    }
+    
+];
