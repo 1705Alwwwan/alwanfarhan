f0c672e688cb35907ef2e1cf34a81933ba7d4f54
Alwan Farhan Aziz <155723244+1705Alwwwan@users.noreply.github.com>
2025-09-12 14:13:56 +0700

    upgrade
    upgrade

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const apps = [
+    {
+        "title": "Bar & QR Code",
+        "creator": "Alwan Farhan Aziz",
+        "file": "https://1705alwwwan.github.io/qrbarccode/",
+        "cover": "media/img/barcode.jpg"
+    },
+    {
+        "title": "Antrian RS",
+        "creator": "Alwan Farhan Aziz",
+        "file": "https://1705alwwwan.github.io/antrian/",
+        "cover": "media/img/antrian.jpg"
+    }
+    
+];
---
+++ js/games.js
@@ -0,0 +1,20 @@
+const games = [
+    {
+        "title": "Memory Games",
+        "creator": "Alwan Farhan Aziz",
+        "file": "https://1705alwwwan.github.io/memory_card/",
+        "cover": "media/img/memory.jpg"
+    },
+    {
+        "title": "Space Invanders",
+        "creator": "Alwan Farhan Aziz",
+        "file": "https://1705alwwwan.github.io/space_invader/",
+        "cover": "media/img/space.jpg"
+    },
+    {
+        "title": "Slot Judi",
+        "creator": "Alwan Farhan Aziz",
+        "file": "https://1705alwwwan.github.io/slot/",
+        "cover": "media/img/slot.jpg"
+    }
+];
