975e30a745546c348f169a30338be09c487715ae
Alwan Farhan Aziz <155723244+1705Alwwwan@users.noreply.github.com>
2025-09-12 16:07:05 +0700

    upgrade
    upgrade

--- js/games.js
+++ js/games.js
@@ -12,6 +12,18 @@ const games = [
         "cover": "media/img/space.jpg"
     },
     {
+        "title": "Tic-tac-toe",
+        "creator": "Alwan Farhan Aziz",
+        "file": "https://1705alwwwan.github.io/tic-tac-toe/",
+        "cover": "media/img/tictactoe.jpg"
+    },
+    {
+        "title": "Snake",
+        "creator": "Alwan Farhan Aziz",
+        "file": "https://1705alwwwan.github.io/snake/",
+        "cover": "media/img/snake.jpg"
+    },
+    {
         "title": "Slot Judi",
         "creator": "Alwan Farhan Aziz",
         "file": "https://1705alwwwan.github.io/slot/",
